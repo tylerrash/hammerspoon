8e94de260fd07879425876dd2d59a70b6e4140d1
Tyler Rash <tyler.rash@gmail.com>
2015-08-16 22:02:24 -0400

    Initial commit

---
+++ init.lua
@@ -0,0 +1,25 @@
+-- mash
+local mash = {"cmd", "shift"}
+
+hs.hotkey.bind(mash, ';', function() hs.grid.snap(hs.window.focusedWindow()) end)
+
+-- Adjust grid size
+hs.hotkey.bind(mash, '=', function() hs.grid.adjustWidth(1) end)
+hs.hotkey.bind(mash, '-', function() hs.grid.adjustWidth(-1) end)
+hs.hotkey.bind(mash, ']', function() hs.grid.adjustHeight(1) end)
+hs.hotkey.bind(mash, '[', function() hs.grid.adjustHeight(-1) end)
+
+-- Move window
+hs.hotkey.bind(mash, 'H', hs.grid.pushWindowLeft)
+hs.hotkey.bind(mash, 'J', hs.grid.pushWindowDown)
+hs.hotkey.bind(mash, 'K', hs.grid.pushWindowUp)
+hs.hotkey.bind(mash, 'L', hs.grid.pushWindowRight)
+
+hs.hotkey.bind(mash, 'N', hs.grid.pushWindowNextScreen)
+hs.hotkey.bind(mash, 'P', hs.grid.pushWindowPrevScreen)
+
+-- Resize Window
+hs.hotkey.bind(mash, 'Y', hs.grid.resizeWindowThinner)
+hs.hotkey.bind(mash, 'U', hs.grid.resizeWindowShorter)
+hs.hotkey.bind(mash, 'I', hs.grid.resizeWindowTaller)
+hs.hotkey.bind(mash, 'O', hs.grid.resizeWindowWider)
